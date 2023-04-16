92d0aa69f34d05527e08b8cfe9bcd704cfc885d6
pravin-pk <19cs119.pravin@sjec.ac.in>
2023-04-16 11:53:31 +0530

    final settings commit

--- insynk/views.py
+++ insynk/views.py
@@ -14,9 +14,9 @@ from firebase_admin import credentials, firestore
 
 load_dotenv()
 
-# cred = credentials.Certificate(os.environ.get('FIREBASE_CREDENTIALS'))
-# firebase_admin.initialize_app(cred)
-firebase_admin.initialize_app()
+cred = credentials.Certificate(os.environ.get('FIREBASE_CREDENTIALS'))
+firebase_admin.initialize_app(cred)
+# firebase_admin.initialize_app()
 db = firestore.client()
 
 # Create your views here.
